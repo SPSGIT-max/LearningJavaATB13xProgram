9b14b6b425dabf410ee14befa3d871abdbf9aaef
SPSGIT-max <sureshspsw@gmail.com>
2025-06-10 08:10:04 +0530

    Updated

---
+++ src/ex_02_Java_Basic02/Lab001_Multiplication_Table.java
@@ -0,0 +1,20 @@
+package ex_02_Java_Basic02;
+
+public class Lab001_Multiplication_Table {
+    public static void main(String[] args) {
+        int number = 7; // Change this to print table of any number
+
+        System.out.println("Multiplication Table of " + number);
+        System.out.println("----------------------------");
+        System.out.println(number + " x 1 = " + (number * 1));
+        System.out.println(number + " x 2 = " + (number * 2));
+        System.out.println(number + " x 3 = " + (number * 3));
+        System.out.println(number + " x 4 = " + (number * 4));
+        System.out.println(number + " x 5 = " + (number * 5));
+        System.out.println(number + " x 6 = " + (number * 6));
+        System.out.println(number + " x 7 = " + (number * 7));
+        System.out.println(number + " x 8 = " + (number * 8));
+        System.out.println(number + " x 9 = " + (number * 9));
+        System.out.println(number + " x 10 = " + (number * 10));
+    }
+}
