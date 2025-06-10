80bb512967ee47345e9fb182a44f0c294732e633
SPSGIT-max <sureshspsw@gmail.com>
2025-06-10 08:06:22 +0530

    Updated

---
+++ src/ex_01_Java_Basic01/Lab002_ShortcutHW.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic01;
+
+public class Lab002_ShortcutHW {
+    public static void main(String[] args) {
+        System.out.println(" Hello Test !!");
+    }
+
+    public static void main(int args) {
+
+    }
+}
---
+++ src/ex_01_Java_Basic01/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic01;
+
+public class Lab007_Comments_ShortCuts {
+    // This code will print the Hi - Ctrl+l
+    // It will print the hi in console
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
